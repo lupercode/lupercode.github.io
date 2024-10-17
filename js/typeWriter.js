0d50116d7e294a2711f10a4ed0655918823e28c8
Luan Pereira <luanpsmorris@gmail.com>
2024-10-17 10:16:04 -0300

    refactor: refatorei o codigo responsavel pelo efeito de digitação no header da página onde as frases agora são obtidas via getAttribute().

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,40 +1,47 @@
-	var i = 0;
-	//var txt = document.getElementsByClassName("typeWriter")[0].innerText;
-	//var t = document.getElementsByClassName("typeWriter")[0].getAttribute('data-type');
-	//var txt = Array(t);
-	//var speed = document.getElementsByClassName("typeWriter")[0].getAttribute('data-period');
-	//var txt = ["Desenvolvedor Front End", "Designer", "Consultor"];
-	var txt = ["Soluções Linux", "Design Gráfico", "Desenvolvimento Web"];
-	var speed = 120;
+document.addEventListener("DOMContentLoaded", () => {
+    const typewriterElement = document.getElementById("typeWriter");
+    const phrases = typewriterElement.getAttribute("data-text").split("|");
+    let phraseIndex = 0;
+    let letterIndex = 0;
+    const typingSpeed = 100; // Velocidade de digitação (ms)
+    const deletingSpeed = 20; // Velocidade de apagar (ms)
+    const pauseBetweenPhrases = 2000; // Pausa entre as frases (ms)
+    const pauseBeforeDeleting = 1000; // Pausa antes de começar a apagar (ms)
 
-	typeWriter();
-	
-	function typeWriter() {
-	var spell = txt[0];
-		if (i < spell.length) {
-	    	document.getElementsByClassName("typeWriter")[0].innerHTML += spell.charAt(i);
-	    	i++;
-	    	setTimeout(typeWriter, speed);
-		}
-		else {
-				txt.shift();
-				setTimeout(del, 700);
-			}
-	}
+    function type() {
+        if (phraseIndex < phrases.length) {
+            const currentPhrase = phrases[phraseIndex];
+            if (letterIndex < currentPhrase.length) {
+                typewriterElement.textContent += currentPhrase.charAt(letterIndex);
+                letterIndex++;
+                setTimeout(type, typingSpeed);
+            } else {
+                // Pausa antes de começar a apagar a frase
+                setTimeout(deleteText, pauseBeforeDeleting);
+            }
+        } else {
+            // Reinicia a animação após todas as frases serem exibidas
+            phraseIndex = 0;
+            type();
+        }
+    }
 
-    function del() {
-		if (i > 0){
-    		var str = document.getElementsByClassName('typeWriter')[0].innerHTML; 
-    		var res = str.substring(0, str.length - 1);
-    		document.getElementsByClassName("typeWriter")[0].innerHTML = res;
-			i--
-			setTimeout(del, 40);
-		}
-			else if (txt.length == 0) {
-					txt = ["Soluções Linux", "Design Gráfico", "Desenvolvimento Web"];
-					setTimeout(typeWriter, 500);
-				}
-		else {
-				setTimeout(typeWriter, 500);
-			}
-	}
+    function deleteText() {
+        const currentPhrase = phrases[phraseIndex];
+        if (letterIndex > 0) {
+            typewriterElement.textContent = currentPhrase.substring(0, letterIndex - 1);
+            letterIndex--;
+            setTimeout(deleteText, deletingSpeed);
+        } else {
+            // Quando terminar de apagar, passa para a próxima frase
+            phraseIndex++;
+            if (phraseIndex >= phrases.length) {
+                phraseIndex = 0; // Reinicia para a primeira frase quando todas tiverem sido mostradas
+            }
+            setTimeout(type, typingSpeed);
+        }
+    }
+
+    type();
+});
+
